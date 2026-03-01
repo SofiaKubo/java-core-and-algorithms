c4129ef70a27710c0417a9eb81bb81a8383648f7
SofiaKubo <skuboreva@gmail.com>
2026-03-01 17:10:12 +0100

    feat(loops): add countdown task solution

---
+++ src/practicum/loops/CountDown.java
@@ -0,0 +1,11 @@
+package practicum.loops;
+
+public class CountDown {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("До старта SpaceY осталось: " + i);
+        }
+        System.out.println("Поехали! Узнаем, есть ли жизнь на Марсе!");
+    }
+}
