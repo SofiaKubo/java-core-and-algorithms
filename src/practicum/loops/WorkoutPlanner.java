62edab3ac63e5f76d112239a4dcc842d2a46cd01
SofiaKubo <skuboreva@gmail.com>
2026-03-02 18:19:52 +0100

    refactor(loops): improve workout planner loop readability

---
+++ src/practicum/loops/WorkoutPlanner.java
@@ -0,0 +1,18 @@
+package practicum.loops;
+
+public class WorkoutPlanner {
+  public static void main(String[] args) {
+    int totalRounds = 5;
+
+    for (int i = 1; i <= totalRounds; i++) {
+      System.out.println("Круг " + i);
+      if (i % 2 == 0) {
+        System.out.println("Качаем пресс");
+      } else {
+        System.out.println("Отжимаемся");
+      }
+      System.out.println("Бегаем в колесе");
+    }
+    System.out.println("Отлично позанимались сегодня! Вы молодец!");
+  }
+}
