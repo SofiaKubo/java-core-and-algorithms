a89ca59414c457dd72524b085d92040a9626ba82
SofiaKubo <skuboreva@gmail.com>
2026-03-02 11:36:49 +0100

    refactor(loops): simplify savings tracker accumulation

---
+++ src/practicum/loops/SavingsTracker.java
@@ -0,0 +1,14 @@
+package practicum.loops;
+
+public class SavingsTracker {
+    public static void main(String[] args) {
+        int days = 5;
+        int moneyPerDay = 200;
+        int moneyTotal = 0;
+
+        for (int day = 1; day <= days; day++) {
+            moneyTotal += moneyPerDay;
+            System.out.println("День " + day + ". Уже " + moneyTotal + " руб.");
+        }
+    }
+}
