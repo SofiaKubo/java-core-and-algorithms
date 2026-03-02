0714f0ed5789961a407993f0849e6cbc7cf1cd89
SofiaKubo <skuboreva@gmail.com>
2026-03-02 17:55:18 +0100

    feat(loops): add karaoke printer task solution

---
+++ src/practicum/loops/KaraokePrinter.java
@@ -0,0 +1,20 @@
+package practicum.loops;
+
+public class KaraokePrinter {
+  public static void main(String[] args) {
+    String doubleFaina = "Фаина Фаина";
+    String fainaNa = "Файна-на";
+    String fainaFainaNa = "Фаина Файна-на";
+
+    for (int line = 1; line <= 10; line++) {
+      if (line == 3 || line == 9) {
+        System.out.println(fainaNa);
+      } else if (line == 4 || line == 10) {
+        System.out.println(fainaFainaNa);
+      } else {
+        System.out.println(doubleFaina);
+      }
+    }
+    System.out.println("Ах люблю тебя, Фаина-Фаина");
+  }
+}
