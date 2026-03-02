6c8870f1f30a4b5af6343f3508f162de218b6221
SofiaKubo <skuboreva@gmail.com>
2026-03-02 17:56:37 +0100

    style(loops): format loop task solutions

--- src/practicum/loops/CountDown.java
+++ src/practicum/loops/CountDown.java
@@ -2,9 +2,9 @@ package practicum.loops;
 
 public class CountDown {
   public static void main(String[] args) {
-      int secondsBeforeStart = 70;
+    int secondsBeforeStart = 70;
 
-    for (int i = secondsBeforeStart; i >= 0; i =  i - 7) {
+    for (int i = secondsBeforeStart; i >= 0; i = i - 7) {
       System.out.println("До старта SpaceY осталось " + i);
     }
     System.out.println("Поехали! Узнаем, есть ли жизнь на Марсе!");
--- src/practicum/loops/LoyaltyProgramCompare.java
+++ src/practicum/loops/LoyaltyProgramCompare.java
@@ -1,34 +1,34 @@
 package practicum.loops;
 
 public class LoyaltyProgramCompare {
-    public static void main(String[] args) {
-        int targetAmount = 40000;
-        simulateTariff("I", targetAmount, 1, 0, true);
-        simulateTariff("II", targetAmount, 100, 100, false);
+  public static void main(String[] args) {
+    int targetAmount = 40000;
+    simulateTariff("I", targetAmount, 1, 0, true);
+    simulateTariff("II", targetAmount, 100, 100, false);
+  }
+
+  private static void simulateTariff(
+      String tariffName, int targetAmount, int initialIncome, int incomeStep, boolean isDoubling) {
+    int day = 1;
+    int dailyIncome = initialIncome;
+    int totalAmount = 0;
+
+    while (totalAmount < targetAmount) {
+      totalAmount += dailyIncome;
+      System.out.println("День " + day + ". На счету " + totalAmount);
+      if (totalAmount >= targetAmount) {
+        break;
+      }
+
+      if (isDoubling) {
+        dailyIncome *= 2;
+      } else {
+        dailyIncome += incomeStep;
+      }
+      day++;
     }
 
-    private static void simulateTariff(
-            String tariffName, int targetAmount, int initialIncome, int incomeStep, boolean isDoubling) {
-        int day = 1;
-        int dailyIncome = initialIncome;
-        int totalAmount = 0;
-
-        while (totalAmount < targetAmount) {
-            totalAmount += dailyIncome;
-            System.out.println("День " + day + ". На счету " + totalAmount);
-            if (totalAmount >= targetAmount) {
-                break;
-            }
-
-            if (isDoubling) {
-                dailyIncome *= 2;
-            } else {
-                dailyIncome += incomeStep;
-            }
-            day++;
-        }
-
-        System.out.println("В варианте " + tariffName + " на " + day
-                + "-й день будет достигнута нужная сумма.");
-    }
+    System.out.println(
+        "В варианте " + tariffName + " на " + day + "-й день будет достигнута нужная сумма.");
+  }
 }
