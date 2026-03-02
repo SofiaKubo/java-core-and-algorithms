e02151cfd061e292e63b7b17f6ba596025d178fe
SofiaKubo <skuboreva@gmail.com>
2026-03-02 18:22:40 +0100

    style(loops): format discount calculator

--- src/practicum/loops/DiscountCalculator.java
+++ src/practicum/loops/DiscountCalculator.java
@@ -1,19 +1,19 @@
 package practicum.loops;
 
 public class DiscountCalculator {
-    public static void main(String[] args) {
-        int totalSum = 0; // переменная для подсчёта суммы
-        int price = 100; // стоимость одного товара
-        int itemAmount = 12; // количество товаров
+  public static void main(String[] args) {
+    int totalSum = 0; // переменная для подсчёта суммы
+    int price = 100; // стоимость одного товара
+    int itemAmount = 12; // количество товаров
 
-        for (int i = 1; i <= itemAmount; i++) {
-            if (i % 4 == 0) {
-                totalSum += price / 2;
-            } else {
-                totalSum += price;
-            }
-        }
-
-        System.out.println("Общая сумма покупок: " + totalSum + " рублей.");
+    for (int i = 1; i <= itemAmount; i++) {
+      if (i % 4 == 0) {
+        totalSum += price / 2;
+      } else {
+        totalSum += price;
+      }
     }
+
+    System.out.println("Общая сумма покупок: " + totalSum + " рублей.");
+  }
 }
