bdfe078d2cb8880903efb56f5b25119d4456eeee
SofiaKubo <skuboreva@gmail.com>
2026-03-03 16:40:38 +0100

    style(loops): format loop exercise files

--- src/practicum/loops/MultiplicationTable.java
+++ src/practicum/loops/MultiplicationTable.java
@@ -3,16 +3,16 @@ package practicum.loops;
 import java.util.Scanner;
 
 public class MultiplicationTable {
-    public static void main(String[] args) {
-        System.out.println("Enter a number");
+  public static void main(String[] args) {
+    System.out.println("Enter a number");
 
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
 
-        for (int i = 1; i <= 10; i++) {
-            int result = number * i;
-            System.out.println(number + " x " + i + " = " + result);
-        }
-        scanner.close();
+    for (int i = 1; i <= 10; i++) {
+      int result = number * i;
+      System.out.println(number + " x " + i + " = " + result);
     }
+    scanner.close();
+  }
 }
--- src/practicum/loops/VowelsCounter.java
+++ src/practicum/loops/VowelsCounter.java
@@ -4,23 +4,23 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class VowelsCounter {
-    private static final String VOWELS = "аеёиоуыэюя";
+  private static final String VOWELS = "аеёиоуыэюя";
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a text:");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a text:");
 
-        String text = scanner.nextLine();
-        int counter = 0;
+    String text = scanner.nextLine();
+    int counter = 0;
 
-        String updatedText = text.toLowerCase(Locale.forLanguageTag("ru"));
+    String updatedText = text.toLowerCase(Locale.forLanguageTag("ru"));
 
-        for (int i = 0; i < updatedText.length(); i++) {
-            if (VOWELS.indexOf(updatedText.charAt(i)) != -1) {
-                counter++;
-            }
-        }
-        System.out.println("Number of vowels: " + counter);
-        scanner.close();
+    for (int i = 0; i < updatedText.length(); i++) {
+      if (VOWELS.indexOf(updatedText.charAt(i)) != -1) {
+        counter++;
+      }
     }
+    System.out.println("Number of vowels: " + counter);
+    scanner.close();
+  }
 }
