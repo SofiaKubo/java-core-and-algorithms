b5e3ff3589b1eab6b408b9d8506f663a4bb63a01
SofiaKubo <skuboreva@gmail.com>
2026-03-03 11:41:39 +0100

    feat(loops): add multiplication table

---
+++ src/practicum/loops/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package practicum.loops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+        scanner.close();
+    }
+}
