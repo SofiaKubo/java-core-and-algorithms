02c556860207ccb858be9fb55b635c59c445bf4e
SofiaKubo <skuboreva@gmail.com>
2026-04-06 13:53:57 +0200

    feat(algorithms/arrays): add array minimum finder

---
+++ src/algorithms/arrays/ArrayMinFinder.java
@@ -0,0 +1,25 @@
+package algorithms.arrays;
+
+import java.util.Scanner;
+
+public class ArrayMinFinder {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int n = console.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+        }
+
+        int min = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+        }
+        System.out.println(min);
+    }
+}
