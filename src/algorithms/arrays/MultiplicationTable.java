32f7c08f7c235eb820554009451ebcc2ffd7a128
SofiaKubo <skuboreva@gmail.com>
2026-04-07 17:52:01 +0200

    refactor(arrays): rename multiplication table local variable

--- src/algorithms/arrays/MultiplicationTable.java
+++ src/algorithms/arrays/MultiplicationTable.java
@@ -2,20 +2,20 @@ package algorithms.arrays;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int[][] MULTIPLICATION_TABLE = new int[10][10];
+        int[][] multiplicationTable = new int[10][10];
 
-        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
-            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
-                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+        for (int i = 0; i < multiplicationTable.length; i++) {
+            for (int j = 0; j < multiplicationTable[i].length; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
             }
         }
 
-        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
-            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
-                if (j < MULTIPLICATION_TABLE[i].length - 1) {
-                    System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+        for (int i = 0; i < multiplicationTable.length; i++) {
+            for (int j = 0; j < multiplicationTable[i].length; j++) {
+                if (j < multiplicationTable[i].length - 1) {
+                    System.out.print(multiplicationTable[i][j] + " ");
                 } else {
-                    System.out.print(MULTIPLICATION_TABLE[i][j]);
+                    System.out.print(multiplicationTable[i][j]);
                 }
             }
             System.out.println();
