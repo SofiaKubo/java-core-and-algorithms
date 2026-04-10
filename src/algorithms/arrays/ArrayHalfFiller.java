5d0a2bb81ca5b7d8381e4f62cd762c060c317c00
SofiaKubo <skuboreva@gmail.com>
2026-04-10 19:09:52 +0200

    feat(arrays): add ArrayHalfFiller

---
+++ src/algorithms/arrays/ArrayHalfFiller.java
@@ -0,0 +1,17 @@
+package algorithms.arrays;
+
+import java.util.Arrays;
+
+public class ArrayHalfFiller {
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        int middle = (array.length + 1) / 2;
+
+        Arrays.fill(array, 0, middle, valueStart);
+        Arrays.fill(array, middle, array.length, valueEnd);
+        System.out.println(Arrays.toString(array));
+    }
+}
