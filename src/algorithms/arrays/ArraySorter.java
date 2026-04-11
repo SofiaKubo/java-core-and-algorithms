228dfaee6917d3c4ba843ba4a3ce71c72821beb5
SofiaKubo <skuboreva@gmail.com>
2026-04-11 17:02:44 +0200

    feat(arrays): add ArraySorter

---
+++ src/algorithms/arrays/ArraySorter.java
@@ -0,0 +1,12 @@
+package algorithms.arrays;
+
+import java.util.Arrays;
+
+public class ArraySorter {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
