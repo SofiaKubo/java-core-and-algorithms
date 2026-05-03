d930a69e93d81d92175a66d7ffd081ac7685a4f7
SofiaKubo <skuboreva@gmail.com>
2026-05-03 16:58:07 +0200

    feat(conditions): add GreetingRobotApp

---
+++ src/core/conditions/GreetingRobotApp.java
@@ -0,0 +1,36 @@
+package core.conditions;
+
+import java.util.Scanner;
+
+public class GreetingRobotApp {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+
+    public static void welcomeUserByName() {
+        System.out.println("Как вас зовут?");
+        String name = scanner.next();
+        System.out.println("Рад познакомиться, " + name + "!");
+    }
+
+    public static void sayHelloByTime() {
+        System.out.println("Который час?");
+        int currentHour = scanner.nextInt();
+
+        if (currentHour >= 22 || currentHour < 6) {
+            System.out.println("Доброй ночи!");
+        } else if (currentHour >= 6 && currentHour < 12) {
+            System.out.println("Доброе утро!");
+        } else if (currentHour >= 12 && currentHour < 18) {
+            System.out.println("Добрый день!");
+        } else if (currentHour >= 18 && currentHour < 22) {
+            System.out.println("Добрый вечер!");
+        }
+
+    }
+
+    public static void printSuccess() {
+        System.out.println("У вас уже неплохо получается программировать!");
+    }
+}
