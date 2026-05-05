d2b55dc3754696030b7b65a8b2dd098728b2efd9
SofiaKubo <skuboreva@gmail.com>
2026-05-05 15:50:54 +0200

    feat(core): add CatBallsCounter exercise

---
+++ src/core/basics/CatBallsCounter.java
@@ -0,0 +1,25 @@
+package core.basics;
+
+import java.util.Scanner;
+
+public class CatBallsCounter {
+    public static void main(String[] args) {
+
+        int balls = 15;
+        System.out.println("У Пикселя " + balls + " мячиков");
+
+        playPixel(balls);
+
+        System.out.println("Пиксель вернул все мячики");
+        System.out.println("Их снова " + balls);
+    }
+
+    public static void playPixel(int balls) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько мячиков спрятал Пиксель?");
+        int  hiddenBalls = scanner. nextInt();
+
+        balls = balls - hiddenBalls;
+        System.out.println("Осталось " + balls);
+    }
+}
