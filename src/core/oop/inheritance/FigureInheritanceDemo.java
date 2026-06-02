475e9237fdd0afd5571022b4de6d8de3b596cfdc
SofiaKubo <skuboreva@gmail.com>
2026-06-02 17:33:06 +0200

    feat(inheritance): fix figure hierarchy design

---
+++ src/core/oop/inheritance/FigureInheritanceDemo.java
@@ -0,0 +1,52 @@
+package core.oop.inheritance;
+
+public class FigureInheritanceDemo {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Square square = new Square();
+        Circle circle = new Circle();
+        Ellipse ellipse = new Ellipse();
+
+        circle.draw();
+        square.draw();
+        rectangle.draw();
+        ellipse.draw();
+        ellipse.circleToEllipse();
+    }
+}
+
+abstract class Figure {
+    public abstract void draw();
+}
+
+class Circle extends Figure {
+    @Override
+    public void draw() {
+        System.out.println("Рисуем круг");
+    }
+}
+
+class Square extends Figure {
+    @Override
+    public void draw() {
+        System.out.println("Рисуем квадрат");
+    }
+}
+
+class Rectangle extends Figure {
+    @Override
+    public void draw() {
+        System.out.println("Рисуем прямоугольник");
+    }
+}
+
+class Ellipse extends Circle {
+    @Override
+    public void draw() {
+        System.out.println("Рисуем овал");
+    }
+
+    public void circleToEllipse() {
+        System.out.println("Превращаем круг в овал");
+    }
+}
