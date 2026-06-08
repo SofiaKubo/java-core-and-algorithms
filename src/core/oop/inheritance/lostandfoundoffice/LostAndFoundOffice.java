0ffbdf67c31e85111c14300a8e27de42da3bf3b3
SofiaKubo <skuboreva@gmail.com>
2026-06-08 15:35:51 +0200

    feat(oop): add lost and found office inheritance example

---
+++ src/core/oop/inheritance/lostandfoundoffice/LostAndFoundOffice.java
@@ -0,0 +1,21 @@
+package core.oop.inheritance.lostandfoundoffice;
+
+import java.util.ArrayList;
+
+public class LostAndFoundOffice {
+    private final ArrayList<Object> things = new ArrayList<>();
+
+    public void put(Object element) {
+        if (element != null) {
+            things.add(element);
+        }
+    }
+
+    public boolean check(Object target) {
+        if (target == null) {
+            return false;
+        }
+
+        return things.contains(target);
+    }
+}
