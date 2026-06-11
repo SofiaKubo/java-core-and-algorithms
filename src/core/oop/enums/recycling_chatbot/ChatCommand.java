e9b59f92f924fd161b8b0fdfcd0c0c99a27fb9bd
SofiaKubo <skuboreva@gmail.com>
2026-06-11 17:35:18 +0200

    feat(oop): add recycling chatbot enum example

---
+++ src/core/oop/enums/recycling_chatbot/ChatCommand.java
@@ -0,0 +1,7 @@
+package core.oop.enums.recycling_chatbot;
+
+public enum ChatCommand {
+  MAP,
+  RECYCLABILITY,
+  BONUS
+}
---
+++ src/core/oop/enums/recycling_chatbot/MaterialType.java
@@ -0,0 +1,9 @@
+package core.oop.enums.recycling_chatbot;
+
+public enum MaterialType {
+  PLASTIC,
+  METAL,
+  GLASS,
+  PAPER,
+  UNSORTED_WASTE
+}
