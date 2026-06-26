37e597ccbeebdefea85e83486df09df6788c509b
SofiaKubo <skuboreva@gmail.com>
2026-06-26 17:26:43 +0200

    test: add bank account junit tests

---
+++ src/test/java/core/testing/junit/bank_account/BankAccountTest.java
@@ -0,0 +1,49 @@
+package core.testing.junit.bank_account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class BankAccountTest {
+
+    @Test
+    public void shouldNotBeBlockedWhenCreated() {
+        BankAccount account = new BankAccount("a", "b");
+        assertFalse(account.isBlocked());
+    }
+
+    @Test
+    public void shouldReturnZeroAmountAfterActivation() {
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("RUB");
+        assertEquals(Integer.valueOf(0), account.getAmount());
+        assertEquals("RUB", account.getCurrency());
+    }
+
+    @Test
+    public void shouldBeBlockedAfterBlockIsCalled() {
+        BankAccount account = new BankAccount("a", "b");
+        account.activate("RUB");
+        account.block();
+        assertTrue(account.isBlocked());
+    }
+
+    @Test
+    public void shouldReturnFirstNameThenSecondName() {
+        BankAccount account = new BankAccount("Anna", "Ivanova");
+        account.activate("RUB");
+        String[] arrayOne = new String[]{"Anna", "Ivanova"};
+        String[] arrayTwo = account.getFullName();
+        assertArrayEquals(arrayOne, arrayTwo);
+    }
+
+    @Test
+    public void shouldReturnNullAmountWhenNotActive() {
+        BankAccount account = new BankAccount("a", "b");
+        assertNull(account.getCurrency());
+    }
+}
